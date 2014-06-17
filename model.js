48834729a20c99205bc4b3351174f3910f2d10f1
sckm <scal.che@gmail.com>
2014-06-17 11:57:55 +0900

    fix

--- model.js
+++ model.js
@@ -4,15 +4,15 @@ var Schema = mongoose.Schema;
 
 // アンケートのタイトル保存・投票受付を行っているか
 var EnqueteSchema = new Schema({
-	title: { type: String, require: true}, 		// アンケートタイトル
-	is_valid: { type: Boolean, require: true}	// 投票が可能か
+	title: { type: String, required: true}, 		// アンケートタイトル
+	is_valid: { type: Boolean, required: true}	// 投票が可能か
 });
 
 // アンケート結果の保存(選択肢+投票数)
 var VoteSchema = {
-	enquete_id: { type: String, require: true},	 // アンケートID
-	choices_name: { type: String, require: true},	// 選択肢名
-	count: { type: Number, require: true}			// 投票数
+	enquete_id: { type: String, required: true},	 // アンケートID
+	choices_name: { type: String, required: true},	// 選択肢名
+	count: { type: Number, required: true}			// 投票数
 }
 
 // mongoDB接続時のエラーハンドリング
